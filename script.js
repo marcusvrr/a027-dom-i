ede4cc1918cb310c9d827ff92747eec37992338d
marcusvrr <marcusvrr2@gmail.com>
2022-09-26 22:05:19 -0300

    exercicios feitos

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const frutas = ["laranja", "limão", "uva"];
+
+let lista1 = document.getElementById("fruta-1")
+lista1.innerHTML = frutas[0]
+
+let lista2 = document.getElementById("fruta-2")
+lista2.innerHTML = frutas[1]
+
+let lista3 = document.getElementById("fruta-3")
+lista3.innerHTML = frutas[2]
+
+let input = document.getElementById('fruta')
+
+function adicionarFruta(){
+    document.getElementById('fruta-4').innerHTML = input.value
+}
